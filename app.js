a5f1dbca73a427bbc06e4ad25afccb73c7549dd1
Radostin Tasev <125496636+RadostinTasev19@users.noreply.github.com>
2023-11-26 17:05:37 +0200

    Add files via upload
    Requester function skeleton

--- app.js
+++ app.js
@@ -1,15 +1,47 @@
-// first we get our root element in which we will write our HTML
-import { contacts } from "./contacts.js"
-import { appTemplate } from "./templates/appTemplate.js"
+import { userData } from "./authentication.js"
+const host = "http://localhost:3030";
+async function requester(url,method,data){
+    const options = {
+        method
+    }
+    if (data) {
+        options.headers = {"Content-Type": "application/json"},
+        options.body = JSON.stringify({data})
+    }
+    const user = userData.getUserData()
+    if (user) {
+        //we create a property x-authorization in options.headers which value will the the user's accessToken
+        options.headers["x-authorization"] = user.accessToken
+    }
 
-const root = document.getElementById("contacts")
-function render(html,parentElement){
-    const div = document.createElement("div")
-    div.classList.add("contact-card")
-    div.innerHTML = html
-    parentElement.replaceChildren(div)
+    try {
+        const response = await fetch(host + url, options)
+        if (response.ok === false) {
+            const error = await response.json()
+            throw new Error(error.message)
+        }
+        if (response.status === 204) {
+            return response
+        }
+
+        return await response.json()
+    } catch (error) {
+        alert(error)
+        throw error
+    }
+
 }
-contacts.forEach((contact) => {
-    debugger
-render(appTemplate(contact),root)
-});
+
+export function get(url){
+    return requester("GET",url)
+}
+
+export function post(url,data){
+    return requester("POST", url,data)
+}
+export function put(url,data){
+    return requester("PUT",url,data)
+}
+export function del(url){
+    return requester("DELETE",url)
+}
